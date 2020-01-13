0944777cc22b4525d6be6546a881529c1f91a66c
Yusuke Umahashi <Yusuke.Umahashi@gmail.com>
2020-01-13 19:49:27 +0900

    Add header for main.

---
+++ src/gb-link.h
@@ -0,0 +1,23 @@
+#define GPIOP_LED GPIOA
+#define GPION_LED GPIO5
+
+#define GPIOP_USART GPIOA
+#define GPION_USART_TX GPIO2
+#define GPION_USART_RX GPIO3
+
+#define GPIOP_SCK GPIOA
+#define GPION_SCK GPIO0
+#define GPIOP_SIN GPIOC
+#define GPION_SIN GPIO0
+#define GPIOP_SOUT GPIOC
+#define GPION_SOUT GPIO1
+#define GPIOP_SD GPIOC
+#define GPION_SD GPIO2
+
+#define UNSET 0x00
+#define MODE_SNIFF 's'
+#define PRINTER_MASTER 'P'
+#define PRINTER_SLAVE 'p'
+
+#define MODE_MASTER 'P'
+#define MODE_SLAVE '1'
